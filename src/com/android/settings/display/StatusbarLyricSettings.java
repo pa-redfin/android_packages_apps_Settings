hentaiOS-redfin/platform_packages_apps_settings

[SQUASHED]Settings: Add status bar lyric
Settings: Include vendor_support

Signed-off-by: QKIvan <ivan33@163.com>

Settings: Add status bar lyric [2/2]

Signed-off-by: Kujou Yuko <i@779.moe>
Change-Id: I944afb9e961eee72ce7fd183f2a97a9210f6c35a

Settings: Reset props for status bar lyric [2/2]

Change-Id: I9d808ed46288849aae5871cef95437208a1d2663
Signed-off-by: Kujou Yuko <i@779.moe>

Co-authored-by: cjybyjk <cjybyjk@zjnu.edu.cn>
Change-Id: I7cbd20074ab70ef6c1813f53ab4499e8c7a79827

src/com/android/settings/display/StatusbarLyricSettings.java
@@ -1,0 +1,40 @@
+/*
+ * Copyright (C) 2022 Project Kaleidoscope
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.display;
+
+import android.os.Bundle;
+
+import androidx.preference.Preference;
+
+import com.android.internal.logging.nano.MetricsProto;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+public class StatusbarLyricSettings extends SettingsPreferenceFragment {
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        addPreferencesFromResource(R.xml.status_bar_lyric_settings);
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.HENTAI_SETTINGS;
+    }
+}

pa-redfin/android_packages_apps_Settings

Change-Id: I16a834ffd9072e7583c0a8ee8059ab08060d8303

src/com/android/settings/display/StatusbarLyricSettings.java
@@ -1,0 +1,40 @@
+/*
+ * Copyright (C) 2022 Project Kaleidoscope
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.display;
+
+import android.os.Bundle;
+
+import androidx.preference.Preference;
+
+import com.android.internal.logging.nano.MetricsProto;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+public class StatusbarLyricSettings extends SettingsPreferenceFragment {
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        addPreferencesFromResource(R.xml.status_bar_lyric_settings);
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.STATUS_BAR_LYRIC;
+    }
+}